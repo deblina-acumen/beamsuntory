582a270cd0ac1cc2fe98b6cb96719a52fd631620
sudip sinha <sinhasudip91@gmail.com>
2020-11-09 09:52:43 +0530

    product edit update (sudip)

--- app/Http/Controllers/product/ProductController.php
+++ app/Http/Controllers/product/ProductController.php
@@ -40,17 +40,83 @@ class ProductController extends Controller
 	{
 		$data=$request->all();
 		$variation_count =isset($data['variation_count']) && $data['variation_count']!=''? $data['variation_count']:0;
-		$attribute = isset($data['attribute']) && $data['attribute']!=''?implode(',',$data['attribute']):'';
-		$attribute_details = ProductAttribute::whereRaw("id in ($attribute)")->orderBy('id','asc')->get();
-		$html='';
 		
+		$variation_value = isset($varience[0]->variation)?json_decode($varience[0]->variation,true):array() ;
+		
+		if(isset($data['product_id'])&& $data['product_id']!='')
+		{
+			$attribute_details = ProductAttribute::orderBy('id','asc')->get();
+			$varience =  ProductVariations::where('item_id',$data['product_id'])->get();
+			$attribute_details = ProductAttribute::orderBy('id','asc')->get();
+			$html='';
+		foreach($varience as $k=>$varience)
+			{
+			$html.='<div class="row">';
 			
+				$varience_value = json_decode($varience->variation,true) ;
+			  foreach($attribute_details as $attributeDetails)
+			{
+				
+			  $attribute_val = isset($attributeDetails->value)&& $attributeDetails->value!=''?explode(',',$attributeDetails->value):array();
+			   if(in_array($attributeDetails->name,array_keys($varience_value))) { 
+              $html.='<div class="col-md-3">';
+			  
+               $html.=' <label>Select '.$attributeDetails->name.'</label>
+                <div class="input-group">
+				
+                <select  aria-controls="project-table" name="variation'.$k.'[]" required class="form-control form-control-sm" onchange="remove_sku('.$k.')">';
+				foreach($attribute_val as $attributeVal){
+					if($varience_value[$attributeDetails->name]==$attributeVal)
+					{
+						$selected="selected";
+					}
+					else{
+						$selected ='' ;
+					}
+                 $html.='<option value="'.$attributeVal.'" '.$selected.'>'.$attributeVal.'</option>';
+				} 
+                 $html.='</select>
+                </div> 
+              </div><input type="hidden" name="attribute_name'.$k.'[]" value="'.$attributeDetails->name.'">
+			  ';
+			  }
+			}
+			
+              $html.='
+			  
+              <div class="col-md-3">
+                <label>SKU</label>
+                <div class="input-group">
+                  <input type="text" class="form-control" value="'.$varience_value['sku'].'" required name="attribute_sku'.$k.'" id="sku'.$k.'">
+                <button type="button" class="btn btn-dark btn-sm" onclick="genarate_sku(this)">Generate SKU</button>
+                </div>
+				<input type="hidden" value="'.$varience->id.'" name="varience_id'.$k.'[]">
+              </div>
+              <div class="col-md-3">
+                <div class="pull-right">
+                                    <label>Action</label>
+                <div class="input-group">
+                  <button type="button" class="btn btn-danger btn-sm" onclick="remove_variation(this)">Remove Variation</button>
+                </div>
+                </div>
+              </div>
+            </div> <hr class="my-15">';
+		}
+		}
+		else{
+			$attribute = isset($data['attribute']) && $data['attribute']!=''?implode(',',$data['attribute']):'';
+		
+		$attribute_details = ProductAttribute::whereRaw("id in ($attribute)")->orderBy('id','asc')->get();
+			$html='';
+		
 			$html.='<div class="row">';
 			foreach($attribute_details as $attributeDetails)
 			{
+				
 			  $attribute_val = isset($attributeDetails->value)&& $attributeDetails->value!=''?explode(',',$attributeDetails->value):array();
-              $html.='<div class="col-md-3">
-                <label>Select '.$attributeDetails->name.'</label>
+              $html.='<div class="col-md-3">';
+			 
+               $html.=' <label>Select '.$attributeDetails->name.'</label>
                 <div class="input-group">
 				
                 <select  aria-controls="project-table" name="variation'.$variation_count.'[]" required class="form-control form-control-sm" onchange="remove_sku('.$variation_count.')">';
@@ -69,6 +135,7 @@ class ProductController extends Controller
                   <input type="text" class="form-control" required name="attribute_sku'.$variation_count.'" id="sku'.$variation_count.'">
                 <button type="button" class="btn btn-dark btn-sm" onclick="genarate_sku(this)">Generate SKU</button>
                 </div>
+				<input type="hidden" value="" name="varience_id'.$variation_count.'[]">
               </div>
               <div class="col-md-3">
                 <div class="pull-right">
@@ -79,20 +146,24 @@ class ProductController extends Controller
                 </div>
               </div>
             </div> <hr class="my-15">';
+			}
+		
+		
 		
 		echo $html;
 	}
     public function save_produt(Request $request)
     {
-        $data=$request->all(); //t();
+        $data=$request->all(); //t($data,1);
 		$have_product = Product::where('name',$data['product_name'])->where('is_deleted','No')->get();
 		if(!empty($have_product) && count($have_product)>0)
 		{
-			return redirect('add-product')->with('error-msg', 'Product Category already exist');
+			return redirect('add-product')->with('error-msg', 'Product  already exist');
 		}
         $insert_data['name']=$data['product_name'];
 		$insert_data['description']=isset($data['product_description'])?$data['product_description']:0;
 		$insert_data['brand_id']=$data['brand'];
+		$insert_data['product_type']=isset($data['product_type'])?$data['product_type']:'';
 		$insert_data['category_id']=$data['category'];
 		$insert_data['supplier_id']=$data['vendor'];
 		$insert_data['regular_price']=$data['regular_price'];
@@ -101,7 +172,7 @@ class ProductController extends Controller
 		$insert_data['low_stock_level']=$data['low_stock_level'];
 		$insert_data['status']=$data['status'];
 		$insert_data['batch_no']='BEAM-'.rand(0,1500).'-'.rand(5,500);
-		//$insert_data['category_id']=$data['shelf_life'];
+		//$insert_data['shelf_life']=$data['shelf_life'];
 		$insert_data['weight']=$data['weight'];
 		$insert_data['length']=$data['length'];
 		$insert_data['width']=$data['Width'];
@@ -133,6 +204,7 @@ class ProductController extends Controller
 				$variation['sku']= $data['attribute_sku'.$i];
 				
 			}
+			
 			$insert_variation['item_id'] = $id;
 			$insert_variation['variation'] = json_encode($variation);
 			$insert_variation['created_by'] = Auth::user()->id;
@@ -150,66 +222,119 @@ class ProductController extends Controller
         }
     }
 
-    public function edit_product_category($id)
+    public function edit_product($id)
     {
+		$data['id'] =$product_id = base64_decode($id);
        
-            $id=base64_decode($id);
-			$data['product_categoryList'] = ProductCategory::where('is_active','Yes')->where('is_deleted','No')->get();
-            $data["title"]="Product Category List";
-            $data["info"]=ProductCategory::where('id',$id)->get();
-            return view('product.ProductCategory.edit',$data);
-       
+            $data['title']="Product category";
+		
+		$data['category']=$list = ProductCategory::where('is_deleted','No')->where('is_active','Yes')->orderBy('id','asc')->get();
+		$data['brand']=$list = Brand::where('is_deleted','No')->where('is_active','Yes')->orderBy('id','asc')->get();
+		$data['supplier']=$list = Supplier::where('is_deleted','No')->where('is_active','Yes')->orderBy('id','asc')->get();
+		$data['product_attribute']=$list = ProductAttribute::where('is_deleted','No')->where('is_active','Yes')->orderBy('name','asc')->get();
+		
+		$data['info'] = $info = Product::where('id',$product_id)->get() ;
+		$data['varience_count'] = $varience =  ProductVariations::where('item_id',$product_id)->count();
+		//echo count($varience);
+		//exit();
+		
+        return view('product.ProductMaster.edit',$data);
         
     }
 
-    public function update_product_category(Request $request)
+    public function update_product(Request $request)
     {
-        $data=$request->all(); //t($data,1);
-		$have_store_category = ProductCategory::where('name',$data['product_category_name'])->where('is_deleted','No')->get();
-		if(!empty($have_store_category) && count($have_store_category)>0 && $have_store_category[0]->id !=  $data["id"] )
+        $data=$request->all();// t($data,1);
+		
+		$have_product = Product::where('name',$data['product_name'])->where('is_deleted','No')->get();
+		if(!empty($have_product) && count($have_product)>0 && $have_product[0]->id !=  $data["id"])
 		{
-			return redirect('edit-Produt-Category/'.base64_encode($data['id']))->with('error-msg', 'Product Category already exist');
+			return redirect('edit-product/'.base64_encode($data['id']))->with('error-msg', 'Product  already exist');
 		}
-        $update_data['name']=$data['product_category_name'];
-		$update_data['parent_id']=isset($data['parent_id'])?$data['parent_id']:'0';
+        $insert_data['name']=$data['product_name'];
+		$insert_data['description']=isset($data['product_description'])?$data['product_description']:0;
+		$insert_data['brand_id']=$data['brand'];
+		$insert_data['product_type']=isset($data['product_type'])?$data['product_type']:'';
+		$insert_data['category_id']=$data['category'];
+		$insert_data['supplier_id']=$data['vendor'];
+		$insert_data['regular_price']=$data['regular_price'];
+		$insert_data['retail_price']=$data['retail_price'];
+		$insert_data['sku']=$data['sku'];
+		$insert_data['low_stock_level']=$data['low_stock_level'];
+		$insert_data['status']=$data['status'];
+		//$insert_data['batch_no']='BEAM-'.rand(0,1500).'-'.rand(5,500);
+		$insert_data['weight']=$data['weight'];
+		$insert_data['length']=$data['length'];
+		$insert_data['width']=$data['Width'];
+		$insert_data['height']=$data['Height'];
+		//upload image2wbmp
+		$cat_image = $request->file('image');
+			if($cat_image !='')
+			{
+				
+					$cat_image_pic_name = upload_file_single_with_name($cat_image, 'product','product',$data['product_name']);	
+					if($cat_image_pic_name!='')
+					{
+						$insert_data['image'] = $cat_image_pic_name;
+					}
+				
+			}
+        $insert_data['updated_by'] = Auth::user()->id;
+		$insert_data['updated_at'] = date('Y-m-d h:i:s');
+       $id = Product::where('id',$data["id"])->update($insert_data);
+	   
+	   
+	    $varience_id_arr = [] ;
+		for($i=0;$i<$data['variation_count'];$i++)
+		{
+			
+			if($data['varience_id'.$i][0]!='')
+			{
+			array_push($varience_id_arr,implode(',',$data['varience_id'.$i]));
+			}
+		}
 		
+		ProductVariations::where('item_id',$data["id"])->whereNotIn('id', $varience_id_arr)->delete(); 
 		
-		$update_data['description']=$data['description'];
-        $update_data['updated_by'] = Auth::user()->id;
-		$update_data['updated_at'] = date('Y-m-d h:i:s');
-        $id=$data["id"];
-        $updated=ProductCategory::where('id',$id)->update($update_data);
-        if($updated)
-            return redirect('product-category-list')->with('success-msg', 'Product Category successfully updated');
-        else
+	 $variation=array();
+		
+		for($i=0;$i<$data['variation_count'];$i++)
+		{
+			$varience_id = $data['varience_id'.$i] ;
+			foreach($data['attribute_name'.$i] as $k=>$variations)
+			{
+				//t($variations);
+				//echo $k;
+				//t($data['variation'.$i][$k]);
+				$variation[$variations] =$data['variation'.$i][$k];
+				$variation['sku']= $data['attribute_sku'.$i];
+				
+			}
+			//t($variation);
+			//t(json_encode($variation));
+			$insert_variation['item_id'] = $data['id'];
+			$insert_variation['variation'] = json_encode($variation);
+			$insert_variation['created_by'] = Auth::user()->id;
+			 if($varience_id[0]!='')
+			{
+				ProductVariations::where('id',$varience_id[0])->update($insert_variation);
+			}
+			else{
+			ProductVariations::insertGetId($insert_variation);
+			}
+		} 
+		
+        if($id!='')
         {
-            $url="edit-Produt-Category/".base64_encode($id);
-            return redirect($url)->with('error-msg', 'Please try after some time');    
+			
+            return redirect('produt-list')->with('success-msg', 'Product Category successfully Updated');
         }
+        else			
+        {
+            return redirect('edit-produt/'.base64_encode($data['id']))->with('error-msg', 'Please try after some time');
+        }
+		
     }
-	public function delete_product_category($id)
-	{
-		 $id= base64_decode($id);
-		 $update_data['is_deleted'] = 'Yes';
-		 $updated=ProductCategory::where('id',$id)->update($update_data);
-        if($updated)
-            return redirect('product-category-list')->with('success-msg', 'Product Category successfully deleted');
-        else
-        {
-            return redirect('product-category-list')->with('error-msg', 'Please try after some time');    
-        }
-	}
-	public function change_status_product_category($id,$status)
-	{
-		$id= base64_decode($id);
-		$update_data['is_active'] = $status;
-		$updated=ProductCategory::where('id',$id)->update($update_data);
-		if($updated)
-            return redirect('product-category-list')->with('success-msg', 'Status successfully changed');
-        else
-        {
-            return redirect('product-category-list')->with('error-msg', 'Please try after some time');    
-        }
-	} 
+	
 }
 ?>
