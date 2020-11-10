06ef2f1ce9251dce517cec98c45d1399b4f6cff6
Ananya1991 <ananya.polley@gmail.com>
2020-11-10 17:51:05 +0530

    po list routes ananya

--- routes/web.php
+++ routes/web.php
@@ -100,6 +100,11 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('add-po-step1/{id?}', 'po\PoMasterController@add');
 	Route::post('get-item-details', 'po\PoMasterController@get_item_details');
 	Route::post('save-po-steop1', 'po\PoMasterController@save_po_step1');
+	
+	Route::any('purchase-order-list', 'purchaseorder\PurchaseOrderController@purchase_order_list');
+	Route::any('purchase-active/{id?}/{value?}', 'purchaseorder\PurchaseOrderController@changeStatus');
+	Route::get('delete-purchase/{id}', 'purchaseorder\PurchaseOrderController@delete_purchase');
+	
 	// Region
 	Route::get('region-master-list', 'Master\RegionController@list');
 	Route::get('add-region', [
