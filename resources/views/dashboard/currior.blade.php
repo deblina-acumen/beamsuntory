efcf2f3d07aecf482e0d39dc76a6ffbda31b4460
sudip sinha <sinhasudip91@gmail.com>
2020-11-18 14:33:52 +0530

    courior dashboard (sudip)

---
+++ resources/views/dashboard/currior.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.master')
+
+@section('header_styles')
+
+<!-- Bootstrap extend-->
+<link rel="stylesheet" href="{{asset('assets/main/css/bootstrap-extend.css')}}">
+
+<!-- Bootstrap 4.1-->
+<link rel="stylesheet" href="{{asset('assets/assets/vendor_components/bootstrap/dist/css/bootstrap.min.css')}}">
+<!-- theme style -->
+<link rel="stylesheet" href="{{asset('assets/main/css/master_style.css')}}">
+<!-- SoftPro admin skins -->
+<link rel="stylesheet" href="{{asset('assets/main/css/skins/_all-skins.css')}}">
+
+@stop
+@section('content')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+      </h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content mob-container">
+		
+      <div class="media flex-column text-center p-40 bg-white mb-30 pull-up">
+          <span class="avatar avatar-xxl bg-success opacity-60 mx-auto">
+          <i class="align-sub fa fa-truck font-size-40" aria-hidden="true"></i>
+          </span>
+          <div class="mt-20">
+            <h4 class="text-uppercase fw-500">Pickup Orders</h4>
+          </div>
+        </div>
+
+        <div class="media flex-column text-center p-40 bg-white mb-30 pull-up">
+          <span class="avatar avatar-xxl bg-blue opacity-60 mx-auto">
+          <i class="align-sub fa fa-truck font-size-40" aria-hidden="true"></i>
+          </span>
+          <div class="mt-20">
+            <h4 class="text-uppercase fw-500">Delivery Orders</h4>
+          </div>
+        </div>
+      
+    </section>
+    <!-- /.content -->
+  </div>
+  
+@stop
+
+@section('footer_scripts')
+
+
+@stop
