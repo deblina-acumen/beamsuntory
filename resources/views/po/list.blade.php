93b1386a5db73eb2ab861eefc6e7f159942a228c
Ananya1991 <ananya.polley@gmail.com>
2020-11-27 22:22:02 +0530

    changes by ananya

--- resources/views/po/list.blade.php
+++ resources/views/po/list.blade.php
@@ -87,7 +87,7 @@
 							<td>{{str_replace('_',' ',ucfirst($list->status))}}</td>
 							<td>{{$list->supplier_name}}</td>
 							<td>{{$list->warehouse_name}}</td>
-							<td>{{ucfirst($list->status)}}</td>
+							<td>{{ucfirst($list->status)}}</td>    
 							<td>
 						  	<?php 
 							if($list->is_active=='Yes') { ?> <a  onclick="return confirm('Are you sure want to Inactive ?')" 
