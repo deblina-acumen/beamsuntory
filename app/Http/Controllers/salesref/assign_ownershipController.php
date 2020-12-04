c6de1e4fa0439cb9debd700423f0746ca46e6b26
sudip sinha <sinhasudip91@gmail.com>
2020-12-05 00:31:34 +0530

    assign ownership (sudip)

--- app/Http/Controllers/salesref/assign_ownershipController.php
+++ app/Http/Controllers/salesref/assign_ownershipController.php
@@ -87,7 +87,7 @@ class assign_ownershipController extends Controller
 			
 		}
 		$where .= " and (stock.type ='each' or stock.type ='shared')" ;
-			$product_list = Product::select('item.name as itemname','item.description','item.image','item.regular_price','item.retail_price','item.batch_no','stock.item_id as stock_item_id','stock.id as stock_id','stock.sku_code','stock.quantity')->join('stock','item.id','=',"stock.item_id")->where('stock.user_id',$user_id)->where('stock.stock_type','in')->whereRaw($where)->groupBy('stock_item_id','stock.sku_code')->get();
+			$product_list = Product::select('item.name as itemname','item.description','item.image','item.regular_price','item.retail_price','item.batch_no','stock.item_id as stock_item_id','stock.id as stock_id','stock.sku_code','stock.quantity','stock.allocation_id','stock.type')->join('stock','item.id','=',"stock.item_id")->where('stock.user_id',$user_id)->where('stock.stock_type','in')->whereRaw($where)->get();
 		
 		
 		
@@ -100,16 +100,315 @@ class assign_ownershipController extends Controller
 		
 	}
 	
-	public function add_allocation($itemId,$skuCode)
+	public function add_allocation($itemid,$skucode,$stockid,$allocationid)
 	{
-		$data['item_id'] = $itemId = base64_decode($itemId);
-		$data['sku_id'] = $skuCode = base64_decode($skuCode);
+		$data['item_id'] = $itemId = base64_decode($itemid);
+		$data['sku_id'] = $skuCode = base64_decode($skucode);
+		$data['stockid'] = $stockid = base64_decode($stockid);
+		$data['allocationid'] = $allocationid = base64_decode($allocationid);
+		
+	$data['product_name'] = $product_name = isset($itemId)?get_product_name_by_id($itemId):'' ;
+		
+	
+	
+		
+	$data['itemId'] =	$itemId = isset($itemId)?$itemId:'';
+	
+	$data['stock_info'] = $stock_info = Stock::where('id',$stockid)->get();
+	$data['quantity'] =	$quantity = $stock_info[0]->quantity ;
+	$data['itemSkuCode'] =	$itemSkuCode = isset($skuCode)?$skuCode:'';
+	$data['puchaseOrderDetailsId'] =	$puchaseOrderDetailsId = isset($po_details_val[0]->puchase_order_details_id)?$po_details_val[0]->puchase_order_details_id:'';
+	
+	$data['userRole'] =	$userRole = Role::where('type','master')->orWhere('type','division')->orWhere('id',11)->get() ;
+	
+	
+return view('salesref.assignownership.add_allocation',$data);
 		
 	}
 	
 	public function submit_assign_allocation(Request $request)
 	{
+		$data=$request->all(); //t($data);
+		 
+		 $userrole2=[];
+		 $userrole5=[];
+		 $userrole9=[];
+		 $userrole11=[];
+		 $count_row = $data['countrow'];
+		 $total_po_quantity = $data['total_quantity'];
+		 $total_quantity = 0 ;
+		 for($sum_count=0;$sum_count<$count_row;$sum_count++)
+		 {
+			 if($data['userrole1_'.$sum_count]==20)
+			 {
+				 if(isset($data['userrole3_'.$sum_count])&&$data['userrole3_'.$sum_count]!='')
+				 {
+					 $mixit_user = $data['userrole3_'.$sum_count];
+				 }else{
+					  $mixit_user = $data['userrole2_'.$sum_count];
+				 }
+				 
+				 $quantity = isset($data['quantity_'.$sum_count])?$data['quantity_'.$sum_count]:'';
+				if(isset($data['eachselectbox_'.$sum_count])&&$data['eachselectbox_'.$sum_count]== 'each')
+				 {
+					$count_mix_user = count($mixit_user);
+					$sum_amount = $count_mix_user * $quantity ;
+				 }
+				 else{
+					 $sum_amount =  $quantity ;
+				 }
+				 
+				$total_quantity = $total_quantity+ $sum_amount ; 
+				 
+			 }
+			 
+			 if($data['userrole1_'.$sum_count]==11)
+			 {
+				 $sales_ref = (isset($data['userrole3_'.$sum_count])&&$data['userrole3_'.$sum_count]!='')?$data['userrole3_'.$sum_count]:array();
+				 $quantity = isset($data['quantity_'.$sum_count])?$data['quantity_'.$sum_count]:'';
+				 
+				 
+				 if((isset($data['eachselectbox_'.$sum_count])&&$data['eachselectbox_'.$sum_count]== 'each')&&(isset($data['storelocator_'.$sum_count])&&$data['storelocator_'.$sum_count]== 'store'))
+				 {
+					  $count_sales_ref_user = count($sales_ref);
+					$sum_amount_each = $count_sales_ref_user * $quantity ;
+					$sum_amount_locker = $count_sales_ref_user * $quantity ; ;
+					$sum_amount = 0 ;
+					 
+				 }
+				 
+				 if((isset($data['eachselectbox_'.$sum_count])&&$data['eachselectbox_'.$sum_count]== 'each')&&(!isset($data['storelocator_'.$sum_count])))
+				 {
+					  $count_sales_ref_user = count($sales_ref);
+					$sum_amount_each = $count_sales_ref_user * $quantity ;
+					$sum_amount_locker = 0 ;
+					$sum_amount = 0 ;
+					 
+				 }
+				  if((isset($data['storelocator_'.$sum_count])&&$data['storelocator_'.$sum_count]== 'store')&&(!isset($data['eachselectbox_'.$sum_count])))
+				 {
+					 $count_sales_ref_user = count($sales_ref);
+					$sum_amount_each = 0 ;
+					$sum_amount_locker = $count_sales_ref_user * $quantity ;
+					$sum_amount = 0 ;
+				 }
+				 if((isset($data['eachselectbox_'.$sum_count])&&$data['eachselectbox_'.$sum_count]== 'each') ||(isset($data['storelocator_'.$sum_count])&&$data['storelocator_'.$sum_count]== 'store'))
+				 {
+				 }
+				 else{
+					$sum_amount =  $quantity ;
+					 $sum_amount_each = 0 ;
+					 $sum_amount_locker = 0 ;
+				 }
+				 
+				 
+				 $total_quantity = $total_quantity+ $sum_amount +$sum_amount_locker+ $sum_amount_each ; 
+			 }
+			  if($data['userrole1_'.$sum_count]==15)
+			 {
+				 
+				  for($j=1;$j<=$data['dynamoselectcount_'.$sum_count];$j++)
+				 {
+					if(isset($data['userrole4_'.$j.'_'.$sum_count])&&$data['userrole4_'.$j.'_'.$sum_count]!='')
+					{
+				    	$field_market_user[$j]= (isset($data['userrole4_'.$j.'_'.$sum_count])&&$data['userrole4_'.$j.'_'.$sum_count]!='')?$data['userrole4_'.$j.'_'.$sum_count]:array();; 
+					}
+				 }
+				 
+				 
+				// $field_market_user = (isset($data['userrole4_'.$data['dynamoselectcount_'.$sum_count].'_'.$sum_count])&&$data['userrole4_'.$data['dynamoselectcount_'.$sum_count].'_'.$sum_count]!='')?$data['userrole4_'.$data['dynamoselectcount_'.$sum_count].'_'.$sum_count]:array(); 
+				 $quantity = isset($data['quantity_'.$sum_count])?$data['quantity_'.$sum_count]:'';
+				if(isset($data['eachselectbox_'.$sum_count])&&$data['eachselectbox_'.$sum_count]== 'each')
+				 {
+					$count_field_market_user = count(end($field_market_user));
+					$sum_amount = $count_field_market_user * $quantity ;
+				 }
+				 else{
+					 $sum_amount =  $quantity ;
+				 }
+				 
+				$total_quantity = $total_quantity+ $sum_amount ; 
+			 }
+			 
+			  if($data['userrole1_'.$sum_count]==5)
+			 {
+				 for($j=1;$j<$data['dynamoselectcount_'.$sum_count];$j++)
+				 {
+					 
+					 if(isset($data['userrole4_'.$j.'_'.$sum_count])&&$data['userrole4_'.$j.'_'.$sum_count]!='')
+					 {
+					$marketing_user[$j]= (isset($data['userrole4_'.$j.'_'.$sum_count])&&$data['userrole4_'.$j.'_'.$sum_count]!='')?$data['userrole4_'.$j.'_'.$sum_count]:array(); 
+					 }
+				 }
+				 
+				 //$marketing_user = (isset($data['userrole4_'.$data['dynamoselectcount_'.$sum_count].'_'.$sum_count])&&$data['userrole4_'.$data['dynamoselectcount_'.$sum_count].'_'.$sum_count]!='')?$data['userrole4_'.$data['dynamoselectcount_'.$sum_count].'_'.$sum_count]:array(); 
+				 $quantity = isset($data['quantity_'.$sum_count])?$data['quantity_'.$sum_count]:'';
+				if(isset($data['eachselectbox_'.$sum_count])&&$data['eachselectbox_'.$sum_count]== 'each')
+				 {
+					$count_marketing_user = count(end($marketing_user));
+					$sum_amount = $count_marketing_user * $quantity ;
+				 }
+				 else{
+					 $sum_amount =  $quantity ;
+				 }
+				 
+				$total_quantity = $total_quantity+ $sum_amount ; 
+			 }
+			 
+		 }
 		
+		 if($total_quantity>$total_po_quantity)
+		 {
+			 //return redirect('add-po-allocation/'.base64_encode($data['itemid']).'/'.base64_encode($data['puchaseOrderDetailsId']).'/'.base64_encode($data['poid']))->with('error-msg', 'Allcation Quantity Not Sufficient');
+		 }
+		 else{
+			 
+			 
+			  for($i=0;$i<$count_row;$i++)
+		 {
+			 //t($data['userrole1_'.$i]);
+			$insertdata['item_id'] = isset($data['itemid'])?$data['itemid']:0 ;
+			$insertdata['po_id'] = isset($data['poid'])?$data['poid']:0 ;
+			$insertdata['podetails_id'] = isset($data['puchaseOrderDetailsId'])?$data['puchaseOrderDetailsId']:0 ;
+			$insertdata['item_sku'] = isset($data['itemSkuCode'])?$data['itemSkuCode']:0 ;
+			
+			
+			
+			  if($data['userrole1_'.$i]==20)
+			 {
+				 $insertdata['role_id'] = $data['userrole1_'.$i];
+				  $insertdata['region_id']= '';
+				  $insertdata['country_id']= '';
+				   $insertdata['brand_id']= '';
+				 $userrole2['roleuser1']= (isset($data['userrole2_'.$i])&&$data['userrole2_'.$i]!='')?implode(',',$data['userrole2_'.$i]):'';
+				 $userrole2['roleuser2']= (isset($data['userrole3_'.$i])&&$data['userrole3_'.$i]!='')?implode(',',$data['userrole3_'.$i]):'';
+				 $insertdata['quantity'] = isset($data['quantity_'.$i])?$data['quantity_'.$i]:'';
+				 if(isset($data['eachselectbox_'.$i])&&$data['eachselectbox_'.$i]== 'each')
+				 {
+					 $insertdata['each_user'] = $data['eachselectbox_'.$i]  ;
+					 $insertdata['share_user'] = '' ;
+					  $insertdata['store_locker'] ='';
+				 }
+				 else{
+					 $insertdata['each_user'] = '';
+					 $insertdata['share_user'] = "shared" ;
+					  $insertdata['store_locker'] ='';
+				 }
+				 $insertdata['user'] = json_encode($userrole2); 
+			 }
+			 if($data['userrole1_'.$i]==5)
+			 {
+				 $insertdata['role_id'] = $data['userrole1_'.$i];
+				 $insertdata['brand_id']= implode(',',$data['userrole2_'.$i]);
+				 
+				  $insertdata['region_id']= '';
+				  $insertdata['country_id']= '';
+				   
+				 
+				  $userrole5['roleuser1']= isset($data['userrole3_'.$i])?implode(',',$data['userrole3_'.$i]):'';
+				 for($j=1;$j<$data['dynamoselectcount_'.$i];$j++)
+				 {
+					 $j1 = $j+1 ;
+					if(isset($data['userrole4_'.$j.'_'.$i])&&$data['userrole4_'.$j.'_'.$i]!='')
+					 {
+					$userrole5['roleuser'. $j1]= (isset($data['userrole4_'.$j.'_'.$i])&&$data['userrole4_'.$j.'_'.$i]!='')?implode(',',$data['userrole4_'.$j.'_'.$i]):''; 
+					 }
+				 }
+				
+				/*  $userrole['roleuser2']= isset($data['userrole4_1_'.$i])?implode(',',$data['userrole4_1_'.$i]):'';
+				 $userrole['roleuser3']= isset($data['userrole4_2_'.$i])?implode(',',$data['userrole4_2_'.$i]):'';
+				 $userrole['roleuser4']= isset($data['userrole4_3_'.$i])?implode(',',$data['userrole4_3_'.$i]):'';
+				 $userrole['roleuser5']= isset($data['userrole4_4_'.$i])?implode(',',$data['userrole4_4_'.$i]):''; */
+				 $insertdata['quantity'] = isset($data['quantity_'.$i])?$data['quantity_'.$i]:'';
+				  if(isset($data['eachselectbox_'.$i])&&$data['eachselectbox_'.$i]== 'each')
+				 {
+					 $insertdata['each_user'] = $data['eachselectbox_'.$i]  ;
+					 $insertdata['share_user'] ='' ;
+					  $insertdata['store_locker'] ='';
+				 }
+				 else{
+					  $insertdata['store_locker'] ='';
+					 $insertdata['each_user'] = '' ;
+					 $insertdata['share_user'] = "shared" ;
+				 }
+				 $insertdata['user'] = json_encode($userrole5); 
+			 } 
+			  if($data['userrole1_'.$i]==15)
+			 {
+				 $insertdata['role_id'] = $data['userrole1_'.$i];
+				 $insertdata['country_id']= implode(',',$data['userrole2_'.$i]);
+				  $insertdata['region_id']= '';
+				 
+				   $insertdata['brand_id']= '';
+				 $insertdata['region_id']= isset($data['userrole3_'.$i])?implode(',',$data['userrole3_'.$i]):'';
+				 
+				  for($j=1;$j<=$data['dynamoselectcount_'.$i];$j++)
+				 {
+					if(isset($data['userrole4_'.$j.'_'.$i])&&$data['userrole4_'.$j.'_'.$i]!='')
+					{
+					$userrole9['roleuser'.$j]= (isset($data['userrole4_'.$j.'_'.$i])&&$data['userrole4_'.$j.'_'.$i]!='')?implode(',',$data['userrole4_'.$j.'_'.$i]):''; 
+					}
+				 }
+				 
+				/*  $userrole['roleuser1']= isset($data['userrole4_1_'.$i])?implode(',',$data['userrole4_1_'.$i]):'';
+				 $userrole['roleuser2']= isset($data['userrole4_2_'.$i])?implode(',',$data['userrole4_2_'.$i]):'';
+				 $userrole['roleuser3']= isset($data['userrole4_3_'.$i])?implode(',',$data['userrole4_3_'.$i]):'';
+				 $userrole['roleuser4']= isset($data['userrole4_4_'.$i])?implode(',',$data['userrole4_4_'.$i]):'';
+				 $userrole['roleuser5']= isset($data['userrole4_5_'.$i])?implode(',',$data['userrole4_5_'.$i]):''; */
+				 $insertdata['quantity'] = isset($data['quantity_'.$i])?$data['quantity_'.$i]:'';
+				 
+				  if(isset($data['eachselectbox_'.$i])&&$data['eachselectbox_'.$i]== 'each')
+				 {
+					 $insertdata['each_user'] = $data['eachselectbox_'.$i]  ;
+					 $insertdata['share_user'] = '' ;
+					  $insertdata['store_locker'] ='';
+				 }
+				 else{
+					  $insertdata['store_locker'] ='';
+					 $insertdata['share_user'] = "shared" ;
+					 $insertdata['each_user'] = '' ;
+				 }
+				 $insertdata['user'] = json_encode($userrole9); 
+			 } 
+			  if($data['userrole1_'.$i]==11)
+			 {
+				 //t($data['userrole3_'.$i]);
+				 $insertdata['role_id'] = $data['userrole1_'.$i];
+				 $insertdata['region_id']= isset($data['userrole2_'.$i])?implode(',',$data['userrole2_'.$i]):'';
+				  $insertdata['country_id']= '';
+				   $insertdata['brand_id']= '';
+				 $userrole11['roleuser1']= (isset($data['userrole3_'.$i])&&$data['userrole3_'.$i]!='')?implode(',',$data['userrole3_'.$i]):'';
+				 $insertdata['quantity'] = isset($data['quantity_'.$i])?$data['quantity_'.$i]:'';
+				  if(isset($data['eachselectbox_'.$i])&&$data['eachselectbox_'.$i]== 'each')
+				 {
+					 $insertdata['each_user'] = $data['eachselectbox_'.$i]  ;
+					 $insertdata['share_user'] ='';
+				 }
+				  if(isset($data['storelocator_'.$i])&&$data['storelocator_'.$i]== 'store')
+				 {
+					 $insertdata['store_locker'] = $data['storelocator_'.$i]  ;
+					 $insertdata['share_user']='';
+				 }
+				 if((isset($data['eachselectbox_'.$i])&&$data['eachselectbox_'.$i]== 'each') ||(isset($data['storelocator_'.$i])&&$data['storelocator_'.$i]== 'store'))
+				 {
+				 }
+				 else{
+					 $insertdata['each_user'] ='';
+					 $insertdata['store_locker'] ='';
+					 $insertdata['share_user'] = "shared" ;
+				 }
+				 $insertdata['user'] = json_encode($userrole11); 
+			 }
+			// t(json_encode($userrole));
+			 
+		 //t($insertdata);
+			// exit() ;
+			 
+		 }
+			 
+			 
+		 }
 	}
+}
 	
 ?>
