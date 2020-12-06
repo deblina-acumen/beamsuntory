c80a6c3c3b984e56ee9f81932305a140380ff35a
Ananya1991 <ananya.polley@gmail.com>
2020-12-06 20:50:51 +0530

    changes by ananya

--- resources/views/salesref/store_delivary/view_delivery_item.blade.php
+++ resources/views/salesref/store_delivary/view_delivery_item.blade.php
@@ -28,47 +28,105 @@
           <!---- List Item ------>
           <div class="box">	
 			<div class="box-body p-0">
+			<div class="row">
+											<div class="col-md-6">
+						<table class="table">
+							<tbody>
+							<tr>
+								<td class="td-heading">Order No :</td>
+								<td class="td-description">{{isset($doinfo[0]->oder_no)?$doinfo[0]->oder_no:''}}</td>
+							</tr>
+							<tr>
+								<td class="td-heading">Created Date :</td>
+								<td class="td-description">{{isset($doinfo[0]->created_at)&& $doinfo[0]->created_at!=''?date('d-m-Y',strtotime($doinfo[0]->created_at)):''}}</td>
+							</tr>
+							<tr>
+								<td class="td-heading">Status :</td>
+								<td class="td-description">{{isset($doinfo[0]->status)?ucwords(str_replace('_',' ',$doinfo[0]->status)):''}}</td>
+							</tr>
+							<tr>
+								<td class="td-heading">Type :</td>
+								<td class="td-description">{{$doinfo[0]->suppler_id ==''? 'Store Delivery':'Ship To Locker'}}</td>
+							</tr>
+							<tr>
+								<td class="td-heading">Store :</td>
+								<td class="td-description">{{isset($store[0]->store_name)?ucwords($store[0]->store_name):''}}</td>
+							</tr>
+							<tr>
+								<td class="td-heading">Store Details :</td>
+								<td class="td-description">Address: {{isset($store[0]->address)?$store[0]->address:''}} <br/>Ph: {{isset($store[0]->phone)?$store[0]->phone:''}} <br/>Country: {{isset($country[0]->country_name)?$country[0]->country_name:''}}<br/>Province: {{isset($province[0]->name)?$province[0]->name:''}}<br/>City: {{isset($store[0]->city)?$store[0]->city:''}}<br/>Zip Code: {{isset($store[0]->zipcode)?$store[0]->zipcode:''}}</td>
+							</tr>
+
+							
+						</tbody></table>
+					</div>
+															<div class="col-md-6">
+						<table class="table">
+							
+							
+							<tbody>
+							<tr>
+								<td class="td-heading">Supplier:</td>
+								<td class="td-description">{{isset($supplier[0]->supplier_name)?$supplier[0]->supplier_name:''}}</td>
+							</tr>
+							<tr>
+								<td class="td-heading">Supplier Details :</td>
+								<td class="td-description">Address: {{isset($supplier[0]->address)?$supplier[0]->address:''}} <br/>Ph: {{isset($supplier[0]->supplier_phone)?$supplier[0]->supplier_phone:''}} <br/>Email: {{isset($supplier[0]->supplier_email)?$supplier[0]->supplier_email:''}}</td>
+							</tr>
+							<tr>
+								<td class="td-heading">Delivery Agent:</td>
+								<td class="td-description">{{isset($delivery_agent[0]->name)?$delivery_agent[0]->name:''}} 
+								{{isset($delivery_agent[0]->lastname) ?$delivery_agent[0]->lastname:0}}</td>
+							</tr>
+							<tr>
+								<td class="td-heading">Delivery Details:</td>
+								<td class="td-description">Address: {{isset($delivery_agent[0]->address)?$delivery_agent[0]->address:''}} <br/>Ph: {{isset($delivery_agent[0]->phone)?$delivery_agent[0]->phone:''}} <br/>Email: {{isset($delivery_agent[0]->email)?$delivery_agent[0]->email:''}} 
+								</td>
+							</tr>
+							
+
+						</tbody></table>
+					</div>
+					</div>
 			<div class="media-list media-list-hover media-list-divided">
-			@if(!empty($do_list)&& count($do_list)>0)	
-			@foreach($do_list as $k=>$doinfo)
+
 		
             <div class="media media-single m-media">
               <div class="media-body">
-			  <div>
-              <h6><?=isset($doinfo->oder_no)?$doinfo->oder_no:'';?></h6>
-              <span><b>Created Date : </b><?=isset($doinfo->created_at) && $doinfo->created_at!='' ? date('d-m-Y',strtotime($doinfo->created_at)):''?></span>
-              <p>
-			  <span><b>Item: </b>
-			  <?= isset($doinfo->store_category)?$doinfo->store_category:''?>
-			  </span>
-			  <br><span><b>SKU:</b> <?= isset($doinfo->store_name)?$doinfo->store_name:''?></span>
-			  <br><span><b>Quantity:</b>&nbsp;</span>
-			  </p>
-			  <span><b>Store Category: </b>
-			  <?= isset($doinfo->store_category)?$doinfo->store_category:''?>
-			  </span>
-			  <br><span><b>Store:</b> <?= isset($doinfo->store_name)?$doinfo->store_name:''?></span>
-			  <br><span><b>Store Information:</b></span>
-			  <br><span>&nbsp;&nbsp;&nbsp; <b>Zip:</b> <?= isset($doinfo->store_zipcode)?$doinfo->store_zipcode:''?></span>
-			  <br><span>&nbsp;&nbsp;&nbsp; <b>Address:</b><?= isset($doinfo->store_address)?$doinfo->store_address:''?></span>
-			   <br><span>&nbsp;&nbsp;&nbsp; <b>Country:</b><?= isset($doinfo->country_name)?$doinfo->country_name:''?></span>
-			    <br><span>&nbsp;&nbsp;&nbsp; <b>Provinces:</b><?= isset($doinfo->provinces_name)?$doinfo->provinces_name:''?></span>
-				 <br><span>&nbsp;&nbsp;&nbsp; <b>City:</b><?= isset($doinfo->store_city)?$doinfo->store_city:''?></span>
-			  <br><span><b>Status:</b>&nbsp;<span class="badge bg-success"> <?= isset($doinfo->status)?ucwords(str_replace('_',' ',$doinfo->status)):''?></span></span>
-			  </p>
-			  <p>
-
-			  </div>
-			  <div class="input-group my-10">
-               
-			   
-			   
-				</div>
+				  <table id="khata_table" class="table table-bordered table-separated">
+					<thead>
+						<tr>
+							<th>Item</th>
+							<th>Batch No.</th>
+						    <th>Expiry Date</th>
+						    <th>Retail Price</th>
+						    <th>Regular Price</th>
+						    <th>Qty</th>
+						    <th>Total</th>
+						</tr>
+					</thead>
+					<tbody>
+					@if(!empty($do_list)&& count($do_list)>0)	
+					@foreach($do_list as $k=>$doinfo)
+						<tr>
+						  <td><div class="pull-left"><img src="{{isset($doinfo->image) && $doinfo->image!=''?URL('public/product/'.$doinfo->image):asset('assets/images/150x100.png')}}" class="user-image rounded-circle b-2" alt="User Image" id="dvPreview" style="height:110px;width:110px"/></div>&nbsp;&nbsp; <span class="td-pic-text">{{isset($doinfo->item_name)?$doinfo->item_name:''}}-{{isset($doinfo->item_sku)?$doinfo->item_sku:''}}</span></td>
+						  <td>{{isset($doinfo->batch_no)?$doinfo->batch_no:''}}</td>
+						  <td>{{isset($doinfo->expire_date)&& $doinfo->expire_date!=''?date('d-m-Y',strtotime($doinfo->expire_date)):''}}</td>
+						  <td>$ {{isset($doinfo->retail_price)?$doinfo->retail_price:''}}</td>
+						  <td>$ {{isset($doinfo->regular_price)?$doinfo->regular_price:''}}</td>
+						  <td>{{isset($doinfo->quantity)?$doinfo->quantity:''}}</td>
+						  <td>$<?php echo $doinfo->quantity * $doinfo->regular_price ; ?></td>
+							</tr>
+							@endforeach
+							@endif
+					
+					</tbody>
+				  </table>
+	
               </div>
 			 </div>
 			
-			@endforeach
-			@endif
+		
             
 				</div>
 			</div>
